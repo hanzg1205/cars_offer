30f1dd71b8ec4c2f7675beb5977d041a7146fdb9
hanzg1205 <1527842986@qq.com>
2019-07-15 17:05:48 +0800

    初始化项目

---
+++ car/src/store/index.js
@@ -0,0 +1,12 @@
+import vue from 'vue';
+import vuex from 'vuex';
+import index from './modules/index';
+
+vue.use(vuex);
+
+let store = new vuex.Store({
+  modules: {
+    index
+  }
+});
+export default store;
